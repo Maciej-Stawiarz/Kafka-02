8059a353ee32798d693c203a87ee0cca8673e98b
Maciej-Stawiarz <precariousDuck@protonmail.com>
2026-04-20 14:43:12 +0200

    feat: Initial commit

---
+++ src/test/java/ms/Kafka_02/Kafka02ApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.Kafka_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kafka02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
